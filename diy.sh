5f936be8afe94c1c77264aff7b4ffc146591583f
asiacny <asiacny@gmail.com>
2019-11-30 15:42:21 +0800

    first commit

---
+++ diy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -r package/lean/luci-app-n2n_v2
+rm -r package/lean/n2n_v2
+
